dceb9396ff748902a4abeac6df1f449ceb532dc5
Jayprakash12345 <0freerunning@gmail.com>
2022-03-27 22:28:42 +0530

    Add defaultRules() and somethingWrong() util function
    Bug: T249000
    Change-Id: I71338f10e4bb169775130259f0899e06992e849f

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-
+from datetime import datetime
 
 def _str(val):
     """
@@ -38,3 +38,49 @@ def badUser():
         "status": "error",
         "msg": "You are not admin of this contest."
     }), 400
+
+
+def somethingWrong():
+    return jsonify({
+        "status": "error",
+        "msg": "Something went wrong."
+    }), 400
+
+
+def defaultRules():
+    return {
+        "namespaceMain": False,
+        "creationInRange": False,
+        "authorOnly": False,
+        "submitterRegDate": {
+            "status": False,
+            "value": datetime.today().strftime('%Y-%m')
+        },
+        "pageSize": {
+            "status": False,
+            "mode": "bytes",
+            "bytes": None,
+            "words": None
+        },
+        "pageSizeBySubmitter": {
+            "status": False,
+            "mode": "bytes",
+            "bytes": None
+        },
+        "minImages": {
+            "status": False,
+            "value": None
+        },
+        "minTemplates": {
+            "status": False,
+            "value": None
+        },
+        "minCategories": {
+            "status": False,
+            "value": None
+        },
+        "minLangLinks": {
+            "status": False,
+            "value": None
+        }
+    }
